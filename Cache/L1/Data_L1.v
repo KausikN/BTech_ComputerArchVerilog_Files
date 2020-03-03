02d8d371f6baa9df72c962bec682843a71e57413
Kausik N <nkausik1999@gmail.com>
2020-03-03 12:56:42 +0530

    changes

--- Cache/L1/Data_L1.v
+++ Cache/L1/Data_L1.v
@@ -1,32 +1,117 @@
+`include "TagCompare.v"
 
 module Data_L1 (
     mode, 
-    WriteAddress, WriteValue, 
-    ReadAddress1, ReadValue1, 
+    WriteAddress_Full, WriteValue, 
+    ReadAddress_Full, ReadValue, 
     reset
 );
 
 input reset;
 input mode; // mode = 0 - Read Mode | mode = 1 - Write Mode
 
-wire [16:1] RegisterFile [4095:0];
+reg [15:0] MainMemory [65535:0];
 
-input [5:1] WriteAddress;
+reg [127:0] DataCache [2047:0];
+reg [2:0] BlockTags [2047:1];
+
+input [16:1] WriteAddress_Full;
 input [16:1] WriteValue;
 
-input [5:1] ReadAddress1;
-output [16:1] ReadValue1;
+input [16:1] ReadAddress_Full;
+output [16:1] ReadValue;
 
-// Write
-always @(*) begin 
-    RegisterFile[WriteAddress] <= mode ? WriteValue : RegisterFile[WriteAddress];
+wire [14:1] WriteAddress;
+wire [14:1] ReadAddress;
+
+wire [11:1] WriteBlockIndex;
+wire [11:1] ReadBlockIndex;
+
+wire [3:1] WriteWordIndex;
+wire [3:1] ReadWordIndex;
+
+wire [2:1] WriteTag;
+wire [2:1] ReadTag;
+
+reg [2047:0] ValidityBit;
+reg [2047:0] DirtyBit;
+
+wire ReadHit;
+wire WriteHit;
+
+// Assign Tag and Indices
+assign WriteTag = WriteAddress_Full[16:15];
+assign ReadTag = ReadAddress_Full[16:15];
+
+assign WriteBlockIndex = WriteAddress_Full[14:4];
+assign ReadBlockIndex = ReadAddress_Full[14:4];
+
+assign WriteWordIndex = WriteAddress_Full[3:1];
+assign ReadWordIndex = ReadAddress_Full[3:1];
+
+assign WriteAddress = WriteAddress_Full[14:1];
+assign ReadAddress = ReadAddress_Full[14:1];
+
+// Initially Validity and Dirty is 0 for all
+initial begin
+    // integer i = 0;
+    // while (i < 2048) begin
+    //     ValidityBit[i] = 1'b0;
+    //     DirtyBit[i] = 1'b0;
+    //     i = i + 1;
+    // end
+    ValidityBit = 2048'b0;
+    DirtyBit = 2048'b0;
 end
 
-// Read 1
-assign ReadValue1 = mode ? ReadValue1 : (RegisterFile[ReadAddress1]);
+// Write
+reg [16:1] TempWriteAddress;
+integer i;
+// Compare Tag
+wire WriteTagCheck;
+TagCompare tcw (WriteTag, BlockTags[WriteBlockIndex], WriteTagCheck);
+assign WriteHit = (WriteTagCheck & ValidityBit[WriteBlockIndex]);
+always @(*) begin 
+    if (WriteHit) begin
+        DataCache[WriteBlockIndex] <= WriteValue;
+        DirtyBit[WriteBlockIndex] <= 1'b1;
+    end
+    else if (!WriteHit) begin
+        TempWriteAddress[14:4] <= WriteBlockIndex;
+        TempWriteAddress[3:1] <= 3'b000;
+        if (DirtyBit[WriteBlockIndex]) begin
+            TempWriteAddress[16:15] <= BlockTags[WriteBlockIndex];
+            i = 0;
+            while (i < 8) begin
+                MainMemory[TempWriteAddress + i][15:0] <= DataCache[WriteBlockIndex][16*i + 15 : 16*i];
+                i = i + 1;
+            end
+            DirtyBit[WriteBlockIndex] = 1'b0;
+        end
+        TempWriteAddress[16:15] <= WriteTag;
+        i = 0;
+        while (i < 8) begin
+            DataCache[WriteBlockIndex][16*i + 15 : 16*i] <= MainMemory[TempWriteAddress + i][15:0];
+            i = i + 1;
+        end
+        DataCache[WriteBlockIndex] <= WriteValue;
+        DirtyBit[WriteBlockIndex] <= 1'b1;
+    end
+end
 
-// always @(*) begin
-//     $display("%b : %b : %b, %b, %b - %b", mode, WriteValue, RegisterFile[1], RegisterFile[2], RegisterFile[3], WriteAddressDecoded);
-// end
+// Read
+// Compare Tag
+wire ReadTagCheck;
+TagCompare tcr (ReadTag, BlockTags[ReadBlockIndex], ReadTagCheck);
+assign ReadHit = (ReadTagCheck & ValidityBit[ReadBlockIndex]);
+assign ReadValue = (ReadTagCheck & ValidityBit[ReadBlockIndex]) ? (mode ? ReadValue : (DataCache[ReadBlockIndex])) :  // Hit
+                                                            (MainMemory[ReadAddress_Full]);                 // Miss
+
+always @(*) begin   // Block Replacement
+    if (!ReadHit) begin
+        DataCache[ReadBlockIndex] <= (MainMemory[ReadAddress_Full]);
+        BlockTags[ReadBlockIndex] <= ReadTag;
+    end
+end
 
 endmodule
---
+++ Cache/L1/TagCompare.v
@@ -0,0 +1,14 @@
+
+module TagCompare (
+    Tag1, Tag2, 
+    CompVal
+);
+
+input [2:1] Tag1;
+input [2:1] Tag2;
+
+output CompVal;
+
+assign CompVal = !(Tag1[1] ^ Tag2[1]) & !(Tag1[2] ^ Tag2[2]);
+
+endmodule
